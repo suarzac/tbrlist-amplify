667d11b9cf852b8aedebd5f777da9b94c81aa881
Zachary Suarez <zacharysuarez@Zacharys-MacBook-Pro.local>
2021-04-28 13:25:08 -0700

    Adding project to my github because thats what people do.

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateBook = /* GraphQL */ `
+  subscription OnCreateBook {
+    onCreateBook {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateBook = /* GraphQL */ `
+  subscription OnUpdateBook {
+    onUpdateBook {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteBook = /* GraphQL */ `
+  subscription OnDeleteBook {
+    onDeleteBook {
+      id
+      name
+      description
+      createdAt
+      updatedAt
+    }
+  }
+`;
